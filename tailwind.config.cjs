ae146c84e8c3498afb93fce140fac5e3d057c38b
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-18 12:29:35 +0300

    fix tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  mode: 'jit',
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {},
