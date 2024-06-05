20bd8e6b1b34e8f07ccfd7c429246ce0e68777d6
illy-dev <illythehase@gmail.com>
2024-06-05 15:57:37 +0200

    updated global css and reformatted Inputfield.tsx

--- src/GUI/Inputfield/Inputfield.tsx
+++ src/GUI/Inputfield/Inputfield.tsx
@@ -1,10 +1,14 @@
 function Inputfield() {
-
     return(
-        <div className="flex flex-col text-white">
-            <p className=" text-2xl">Test</p>
+        <div className="flex flex-col justify-center items-center text-white pt-10">
 
-            <input type="text" name="key_input" id="key_input" className="bg-white appearance-none border border-gray-400 rounded-full w-[20%] py-4 px-4 text-black leading-tight focus:outline-none focus:border-indigo-500 focus:border-[2px] text-center" placeholder="Enter Code" required></input>
+            <p className=" text-2xl">Text Field</p>
+            <input  type="text"
+                    name="key_input"
+                    id="key_input"
+                    className="bg-white appearance-none border border-gray-400 rounded-full w-[15%] p-4 text-black leading-tight focus:outline-none focus:border-indigo-500 focus:border-[2px] text-center mt-3"
+                    placeholder="Enter Code"
+                    required></input>
 
         </div>
     );
