af07ca24c4c2e6f7ae9c79069c695a968067b1be
illy-dev <illythehase@gmail.com>
2024-06-10 21:57:56 +0200

    smh still not working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use thiserror::Error;
+use tauri::InvokeError;
+
+#[derive(Error, Debug)]
+enum ReqwestError {
+    #[error("HTTP request error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("Unexpected error: {0}")]
+    Other(String),
+}
+
+impl From<ReqwestError> for InvokeError {
+  fn from(error: ReqwestError) -> Self {
+      InvokeError::from(format!("{}", error))
+  }
+}
+
 #[tauri::command]
-fn get_book(code: &str) -> Result<String, Box<dyn std::error::Error>> {
+fn get_book(code: &str) -> Result<String, ReqwestError> {
   let url = format!("https://codes.cornelsen.de/codes/webcode-ws/api/v1/searchAll?searchterm={}", code);
-  let resp: String = reqwest::blocking::get(&url)?.text()?;
+  let resp = reqwest::blocking::get(&url)?.text()?;
   Ok(resp)
 }
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![get_book])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import Inputfield from "./GUI/main/Inputfield";
 
-function App() {
+import { invoke } from '@tauri-apps/api/tauri'
 
+async function get_book() {
+  // const book = document.querySelector('#book')!;
+  // book.textContent = await invoke('get_book', { code: 'examplecode'});
+  // const renderedText = htmlElement.innerText;
+  console.log(await invoke('get_book', { code: 'examplecode'}));
+}
+
+function App() {
   return (
     <div className="text-black h-[100%] w-[100%]">
       <Inputfield />
+      <p id="book"></p>
     </div>
   );
 }
 
 export default App;
 // westermann.de/webcodes/webcode={code}
-// codes.cornelsen.de/codes/webcode-ws/api/v1/searchAll?searchterm=9783061209735
+// codes.cornelsen.de/codes/webcode-ws/api/v1/searchAll?searchterm={code}
